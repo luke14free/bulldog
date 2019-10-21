9df62544707b7711b219bab471d9a78fb6561a69
Luca <luca.giacomel@gmail.com>
2019-10-21 13:23:47 +0200

    fix: add pathos install requirement

--- setup.py
+++ setup.py
@@ -19,5 +19,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['pathos'],
     python_requires='>=3.6',
 )
